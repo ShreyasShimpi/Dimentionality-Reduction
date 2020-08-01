215e08b37dadd0b5e9c19f20ded9337be056b1bc
ShreyasShimpi <48646807+ShreyasShimpi@users.noreply.github.com>
2020-08-01 17:20:34 +0530

    Add files via upload

--- pca.py
+++ pca.py
@@ -1,307 +1,81 @@
-
-# coding: utf-8
-
-# In[119]:
-
-
+# !/usr/bin/env python
+"""This script prompts a mathematical approch on Principle Component Analysis"""
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-# # IMPUTATION
-
-# In[120]:
-
-
-df = pd.read_csv('Database_for_dimensionality_reduction.csv')
-dfcopy = df.copy()
-dfcopy1= df.select_dtypes(include=[np.number])
-dfcopy1.head()
-#df.columns[2]
-
-
-# In[121]:
-
-
-dfcopy1.shape
-
-
-# In[122]:
-
-
-dfcopy1.isnull().sum()
-
-
-# In[123]:
-
-
-for column in dfcopy1: 
-        meani = dfcopy[column].mean()
-        dfcopy1[column].fillna(meani, inplace=True)
-
-
-# In[124]:
-
-
-dfcopy1.isnull().sum()
-
-
-# In[125]:
-
-
-dfcopy1.head()
-
-
-# In[126]:
-
-
-df_act = dfcopy1[["AP1","AP2","DT1","DT2","HTM","LTM","AC1","AC2","LC1","LC2","RFL","RH1","RH2","WSD"]]
-
-
-# In[127]:
-
-
-df_act.head()
-
-
-# In[128]:
-
-
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
+# Reading the database
+df = pd.read_csv('..\\Database_for_dimensionality_reduction.csv')
+dfcopy = df.select_dtypes(include=[np.number])
+print("This is the imported database.")
+dfcopy.head()
 
-# In[129]:
+# Data Imputation
+for column in dfcopy:
+    meani = dfcopy[column].mean()
+    dfcopy[column].fillna(meani, inplace=True)
 
+df_act = dfcopy
 
-sc= StandardScaler()
+# Data Standardization
+sc = StandardScaler()
 df_scale = sc.fit_transform(df_act)
-df_scale.shape
-
-
-# In[130]:
-
-
-df_scale = pd.DataFrame(df_scale, columns = df_act.columns)
+df_scale = pd.DataFrame(df_scale, columns=df_act.columns)
+print("This is the scaled database.")
 df_scale.head()
 
+# Data Splitting in the Label and Feature Database
 
-# ## Choosing AP1 as target function label
-
-# In[131]:
-
-
+# This is Feature/target variable
 Y_label = df_scale["AP1"]
-
-
-# In[132]:
-
-
-df_scale.drop("AP1", axis = 1, inplace = True)
-
-
-# In[133]:
-
-
+df_scale.drop("AP1", axis=1, inplace=True)
+# This is label database.
 X_label = df_scale
+print("This is label database")
 X_label.head()
 
+# PCA on Feature Database
 
-# ## PCA ON DATABASE
+X_cov = X_label.T.dot(X_label) / (len(df) - 1)
+eig_val, eig_vec = np.linalg.eig(X_cov)
 
-# In[ ]:
+Sum = eig_val / sum(eig_val) * 100
+cum_sum = np.cumsum(Sum)
+# Scree plot of cumulative sum
+fig = plt.figure()
 
-
-#X_cov2 = np.cov(X_label.T)
-
-
-# In[135]:
-
-
-X_cov = (X_label.T).dot(X_label)/ (len(df) -1)
-X_cov
-
-
-# In[136]:
-
-
-eigval, eigvec = np.linalg.eig(X_cov)
-
-
-# In[137]:
-
-
-eigval
-
-
-# In[185]:
-
-
-sum = eigval/sum(eigval)*100
-
-
-# In[140]:
-
-
-cumsum = np.cumsum(sum)
-cumsum
-
-
-# In[141]:
-
-
-plt.plot(cumsum)
+x = range(1, len(cum_sum)+1)
+plt.plot(x, cum_sum)
+plt.gca().invert_yaxis()
+plt.xlabel("k")
+plt.ylabel("Cum. Sum of Eigenvalues")
 plt.show()
 
-
-# In[142]:
-
-
-#taking 90% of the actual data
-for i in range(len(cumsum)):
-    if (cumsum[i] >= 90):
-        k = i;
+# taking 85% of the actual data
+k = 0
+for i in range(len(cum_sum)):
+    if cum_sum[i] >= 85:
+        k = i + 1
         break
-k
+print(k)
 
-
-# In[198]:
-
-
-#top 5 eigen values
-X_5 = eigvec[0:5,:]
-X_5.shape
-
-
-# In[197]:
-
-
+# top 5 eigenvalues since k is equal to 5
+X_5 = eig_vec[0:5, :]
 X_tran = X_label.dot(X_5.T)
-
-
-# In[203]:
-
-
-#X_tran.columns = ["PC1","PC2","PC3","PC4","PC5"]
+X_tran.columns = ["PC1", "PC2", "PC3", "PC4", "PC5"]
 X_tran["label"] = Y_label
+print("This is the transformed database into 5 Principle Components.")
 X_tran.head()
 
+# validation using PCA function in sklearn
 
-# In[222]:
-
-
-plt.figure(figsize=(8,6))
-plt.scatter(X_tran.iloc[:,0], X_tran.iloc[:,1] ,c = X_tran.iloc[:,-1],cmap='rainbow')
-plt.show()
-
-
-# ## MEAN of DT1 and DT2 as a lebel 
-
-# In[147]:
-
-
-#df_act = dfcopy1[["AP1","AP2","DT1","DT2","HTM","LTM","AC1","AC2","LC1","LC2","RFL","RH1","RH2","WSD"]]
-#df_act.head()
-
-
-# In[148]:
-
-
-# sc= StandardScaler()
-# df_scale = sc.fit_transform(df_act)
-
-
-# In[149]:
-
-
-#df_scale = pd.DataFrame(df_scale, columns = df_act.columns)
-
-
-# In[150]:
-
-
-# Y_label = (df_scale["DT1"] + df_scale["DT2"])/2
-
-
-# In[151]:
-
-
-# df_scale.drop(["DT1","DT2"], axis=1, inplace = True)
-
-
-# In[152]:
-
-
-# X_label = df_scale
-# X_label.head()
-
-
-# In[153]:
-
-
-# X_cov = np.cov(X_label.T)
-# eigval, eigvec = np.linalg.eig(X_cov)
-
-# #top 5 eigen values
-# X_5 = eigvec[0:5,:]
-# X_5.shape
-
-# X_tran = X_label.dot(X_5.T)
-
-# X_tran.columns = ["PC1","PC2","PC3","PC4","PC5"]
-# X_tran["Y_label"] = Y_label
-
-
-# In[154]:
-
-
-# X_tran.head()
-
-
-# In[155]:
-
-
-# plt.figure(figsize=(8,6))
-# plt.scatter(X_tran.iloc[:,0], X_tran.iloc[:,-1], c = X_tran.iloc[:,-1],cmap='rainbow')
-# plt.show()
-
-
-# ## with pca library
-
-# In[156]:
-
-
-from sklearn.decomposition import PCA
 pca = PCA(n_components=5)
 pca.fit(X_label)
 X_pca = pca.transform(X_label)
-
-
-# In[157]:
-
-
 X_pca = pd.DataFrame(X_pca)
-
-X_pca["Y_label"] = Y_label
+X_pca["X_label"] = Y_label
+X_pca.columns = ["PC1", "PC2", "PC3", "PC4", "PC5", "X_label"]
+print("This is data for validation.")
 X_pca.head()
-
-
-# In[158]:
-
-
-X_tran.head()
-
-
-# In[159]:
-
-
-plt.figure(figsize=(8,6))
-plt.scatter(X_pca.iloc[:,0], X_pca.iloc[:,1], c = X_pca.iloc[:,-1],cmap='rainbow')
-plt.show()
-
-
-# In[ ]:
-
-
-
-
--- svd.py
+++ svd.py
@@ -1,262 +1,91 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
+# !/usr/bin/env python
+"""This script prompts a mathematical approch on Principle Component Analysis"""
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import TruncatedSVD
 
+# Reading the database
+df = pd.read_csv('..\\Database_for_dimensionality_reduction.csv')
+dfcopy = df.select_dtypes(include=[np.number])
+print("This is the imported database.")
+dfcopy.head()
 
-# In[2]:
+# Data Imputation
+for column in dfcopy:
+    meani = dfcopy[column].mean()
+    dfcopy[column].fillna(meani, inplace=True)
 
+df_act = dfcopy
 
-df = pd.read_csv('Database_for_dimensionality_reduction.csv')
-dfcopy = df.copy()
-dfcopy1= df.select_dtypes(include=[np.number])
-dfcopy1.head()
-
-
-# In[3]:
-
-
-for column in dfcopy1: 
-        meani = dfcopy[column].mean()
-        dfcopy1[column].fillna(meani, inplace=True)
-dfcopy1.head()
-
-
-# In[4]:
-
-
-df_act = dfcopy1[["AP1","AP2","DT1","DT2","HTM","LTM","AC1","AC2","LC1","LC2","RFL","RH1","RH2","WSD"]]
-df_act.head()
-
-
-# ## scaling
-
-# In[5]:
-
-
-sc= StandardScaler()
+# Data Standardization
+sc = StandardScaler()
 df_scale = sc.fit_transform(df_act)
-df_scale = pd.DataFrame(df_scale, columns = df_act.columns)
+df_scale = pd.DataFrame(df_scale, columns=df_act.columns)
+print("This is the scaled database.")
 df_scale.head()
 
+# Data Splitting in the Label and Feature Database
 
-# ## splitting into label and feature
-
-# In[6]:
-
-
+# This is Feature/target variable
 Y_label = df_scale["AP1"]
-Y_label[:5]
-
-
-# In[7]:
-
-
-df_scale.drop("AP1", axis = 1, inplace = True)
+df_scale.drop("AP1", axis=1, inplace=True)
+# This is label database.
 X_label = df_scale
-#X_label.head()
-
-
-# In[8]:
-
-
+print("This is label database")
 X_label.head()
 
+# SVD on Feature Database
 
-# # SVD
-
-# In[9]:
-
-
-U, S, Vt = np.linalg.svd(X_label)
-
-
-# In[10]:
-
-
-S
-
-
-# In[11]:
-
-
-# Cum = S/S.sum()*100
-# Cum = Cum.cumsum()
-# k=0
-# for i in range(len(Cum)):
-#     if (Cum[i] >= 90):
-#         k = i;
-#         break
-# k, Cum
-
-
-# In[12]:
-
-
-k = 5
-
-
-# In[44]:
-
-
-S1 = np.zeros((X_label.shape[0], X_label.shape[1]))
-S1[:X_label.shape[1], :X_label.shape[1]] = np.diag(S)
-
-
-# In[45]:
-
-
-S1.shape
-
-
-# In[15]:
-
-
-Vt.shape,U.shape
-
-
-# In[46]:
-
-
-Vt1 = Vt[:5,:]
-S11 = S1[:,:5]
-S11.shape, Vt1.shape
-
-
-# In[59]:
-
-
-X_label_Reconstruct = U.dot(S11.dot(Vt1))
-
-
-# In[60]:
-
-
-X_label_Reconstruct = pd.DataFrame(X_label_Reconstruct, columns = X_label.columns)
-X_label_Reconstruct.head()
-
-
-# In[68]:
-
-
-X_label_Transformed = X_label_Reconstruct.dot(Vt1.T)
-X_label_Transformed.head()
-
-
-# # SVD MATHEMATICAL
-
-# In[19]:
-
-
-temp1 = (X_label.T).dot(X_label)
-temp2 = (X_label).dot(X_label.T)
-
-
-# In[20]:
-
-
-temp1.shape, temp2.shape
-
-
-# In[21]:
-
+temp1 = X_label.T.dot(X_label)
+temp2 = X_label.dot(X_label.T)
 
 eigval_V, eigvec_V = np.linalg.eig(temp1)
 eigval_U, eigvec_U = np.linalg.eig(temp2)
 
+temp3 = eigval_V ** 0.5
 
-# In[22]:
+Cum = temp3 / temp3.sum() * 100
+Cum = Cum.cumsum()
+# Scree plot of cumulative sum
+fig = plt.figure()
 
+x = range(1, len(Cum)+1)
+plt.plot(x, Cum)
+plt.gca().invert_yaxis()
+plt.xlabel("k")
+plt.ylabel("Cum. Sum of Eigenvalues")
+plt.show()
 
-eigvec_U.shape, eigvec_V.shape
-
-
-# In[29]:
-
-
-temp3 = eigval_V**0.5
-
-
-# In[31]:
-
+# taking 60% of the actual data
+k = 0
+for i in range(len(Cum) - 1):
+    if Cum[i] >= 60:
+        k = i + 1
+        break
+print(k)
 
 Sigma = np.zeros((X_label.shape[0], X_label.shape[1]))
 Sigma[:X_label.shape[1], :X_label.shape[1]] = np.diag(temp3)
-Sigma.shape
 
+# The value of k is 5
+Vt_m = eigvec_V.T[:5, :]
+Sigma = Sigma[:, :5]
 
-# In[82]:
-
-
-# we can see that S ans Sigma are same....
-#we will take k = 5
-Vt_m = eigvec_V.T[:5,:]
-Sigma = Sigma[:,:5]
-Sigma.shape, V_m.shape
-
-
-# In[95]:
-
-
-eigvec_U[:1,:20].real
-
-
-# In[96]:
-
-
-U[:1,:20]
-
-
-# In[99]:
-
-
-X_label_Recon_m = (eigvec_U).dot(Sigma.dot(Vt_m))
-X_label_Recon_m = pd.DataFrame(X_label_Recon_m.real, columns = X_label.columns)
-X_label_Recon_m.head()
-
-
-# In[101]:
-
+X_label_Recon_m = eigvec_U.dot(Sigma.dot(Vt_m))
+X_label_Recon_m = pd.DataFrame(X_label_Recon_m.real, columns=X_label.columns)
 
 X_label_Trans_m = X_label_Recon_m.dot(Vt_m.T)
 X_label_Trans_m.head()
 
-
-# ## SVD with SKlearn
-
-# In[65]:
-
-
-from sklearn.decomposition import TruncatedSVD
+# validation using TruncatedSVD function in sklearn
 
 svd = TruncatedSVD(n_components=5)
 svd.fit(X_label)
 result = svd.transform(X_label)
 result = pd.DataFrame(result)
-
-
-# # The Results Compared
-
-# In[103]:
-
-
-result.head()   #This is TruncatedSVD sklearn lib result
-
-
-# In[66]:
-
-
-X_label_Transformed.head() #This is using numpy SVD 
-
-
-# In[102]:
-
-
-X_label_Trans_m.head()    #This is a Complete Mathematical Approch
-
+result.columns = ["PC1", "PC2", "PC3", "PC4", "PC5"]
+result["X_label"] = Y_label
+print("This is data for validation.")
+result.head()
